f472470edf826fa1ac1388991cd6bfce51aa0acc
Miasya Bulger <miasya.bulger@mail.mcgill.ca>
2020-06-15 21:34:39 +0000

    intermediate pseudocode for FindMeetingQuery. more to be addedgit add . committed solely to show ideation progress so far.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,71 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import com.google.sps.TimeRange;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    
+    Collection<TimeRange> blockedTimes = new ArrayList<TimeRange>();
+
+    // Get important request information
+    Collection<String> attendees = request.getAttendees();
+    long duration = request.getDuration();
+
+    // Base case: No attendees:
+    if (attendees.size() == 0){
+
+    }
+
+    // For all events in event
+    for (Event e : events){
+
+      // Check if at least one request attendee is an event attendee, otherwise skip event
+      ArrayList<String> eventAttendees = new ArrayList<String>(e.getAttendees());
+      eventAttendees.retainAll(attendees);
+      if (eventAttendees.size() != 0){
+
+        // Insert the time to list of blockTimes
+        eventTime = e.getWhen();
+
+        for (TimeRange t : blockedTimes){
+          // if overlap, check if start or duration of blocked region grows
+          
+          // if need to adjust start time
+          if (eventTime.getStartTime() + eventTime.getDuration() > t.getStartTime()){
+            
+            blockedTimes.add(TimeRange.fromStartEnd(eventTime.getStartTime(), t.getStartTime() + t.getDuration(), false));
+            // remove t in a safer way than blockedTimes.remove(t); cause iterating
+          }
+
+          // if need to adjust end time
+          if (){
+
+          }
+
+          // if no overlap, don't merge, just add it
+          if (){
+            blockedTimes.add(eventTime);
+          }
+        }
+
+      }
+    }
+
+    // Sort by start time (not the most efficient, but works for our purposes)
+    //Collections.sort(blockedTimes);
+
+
+    // Look through all possible times for a duration of sufficient length
+    Collection<TimeRange> meetingTimes = new ArrayList<TimeRange>();
+
+    // take difference?
+
+    // Return meeting times (could be empty)
+    return meetingTimes;
   }
 }
